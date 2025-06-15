e28035cb11ecd99940cb916c35b0c55a7d49f9eb
Knight-project <vallrayn26@gmail.com>
2025-06-16 00:03:54 +0600

    fuck  you

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nyxeros.cursebreakercodex',
+  appName: 'Cursebreaker_codex',
+  webDir: 'out'
+};
+
+export default config;
